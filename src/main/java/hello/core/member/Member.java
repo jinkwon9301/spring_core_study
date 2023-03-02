19c2185a811b84acccc54ca574b424a0ab03d6e6
jinkwon <valor9254@gmail.com>
2023-03-02 16:34:20 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,37 @@
+package hello.core.member;
+
+public class Member {
+  private String id;
+  private String name;
+  private Grade grade;
+
+  public Member(String id, String name, Grade grade) {
+    this.id = id;
+    this.name = name;
+    this.grade = grade;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Grade getGrade() {
+    return grade;
+  }
+
+  public void setGrade(Grade grade) {
+    this.grade = grade;
+  }
+}
