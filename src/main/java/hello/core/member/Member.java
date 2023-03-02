88204d245d5955ab66920181a4acf5ea369efc47
jinkwon <valor9254@gmail.com>
2023-03-02 17:03:56 +0900

    회원 도메인 실행과 테스트

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -1,21 +1,21 @@
 package hello.core.member;
 
 public class Member {
-  private String id;
+  private Long id;
   private String name;
   private Grade grade;
 
-  public Member(String id, String name, Grade grade) {
+  public Member(Long id, String name, Grade grade) {
     this.id = id;
     this.name = name;
     this.grade = grade;
   }
 
-  public String getId() {
+  public Long getId() {
     return id;
   }
 
-  public void setId(String id) {
+  public void setId(Long id) {
     this.id = id;
   }
 
